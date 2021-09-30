71ac0daf09c58a1a76243a237676da7dd9077d09
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-09-30 14:17:59 +0300

    Adds Feedback component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
 import { Component } from 'react';
 import { Section } from 'components/Section/Section';
-import { Statistics } from 'components/Statistics/Statistics';
+import Statistics from 'components/Statistics/Statistics';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 
 export default class App extends Component {
   state = {
@@ -10,12 +11,35 @@ export default class App extends Component {
     bad: 0
   };
 
+  handleFeedback = event => {
+    const { name } = event.currentTarget;
+
+    // const validFeedbackTypes = Object.keys(this.state).filter(
+    //   key => name === key,
+    // );
+    // if (!validFeedbackTypes.length) return;
+
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+
   render() {
     return (
       <div>
-        <Section title="Please, leave feedback" className="feedback" />
-        <Statistics />
+        <Section
+          title="Please, leave feedback"
+          className="feedback" />
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.handleFeedback} />
+        <Statistics
+          state={this.state} />
       </div>
     )
   }
 }
+
